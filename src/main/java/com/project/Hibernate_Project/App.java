55d59923d96a0349b0d58cebb5cb600eaa2e152e
Akshay-06 <akshayreddy1999@gmail.com>
2024-06-12 22:07:21 -0400

    Added code for storing data in DB using Hibernates

--- src/main/java/com/project/Hibernate_Project/App.java
+++ src/main/java/com/project/Hibernate_Project/App.java
@@ -1,13 +1,62 @@
 package com.project.Hibernate_Project;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		
+		// One Way to do it
+//    	Alien alien = new Alien();
+//    	alien.setAid(2);
+//    	alien.setName("Samhitha");
+//    	alien.setColour("Black");
+//    	
+//    	Configuration cf = new Configuration().configure().addAnnotatedClass(Alien.class);
+//    	
+//
+//    	
+//        SessionFactory sf = cf.buildSessionFactory();
+//        
+//        Session session = sf.openSession();
+//        
+//        Transaction tx = session.beginTransaction();
+//        
+//        session.save(alien);
+//        
+//        tx.commit();
+
+		
+		// Otherway to do it
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+
+		Alien e1 = new Alien();
+		e1.setAid(3);
+		e1.setName("Siva");
+		e1.setColour("White");
+
+		session.persist(e1);
+		t.commit();
+		System.out.println("successfully saved");
+		factory.close();
+		session.close();
+
+	}
 }
